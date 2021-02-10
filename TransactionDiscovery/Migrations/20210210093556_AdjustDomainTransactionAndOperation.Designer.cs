0df8ba6988e6ace72b601958be2f00ec3adede49
Illia Pivtoraiko <illia_pivtoraiko@epam.com>
2021-02-10 11:50:15 +0200

    Add migration for adjusting domain

--- TransactionDiscovery/Migrations/20210210093556_AdjustDomainTransactionAndOperation.Designer.cs
+++ TransactionDiscovery/Migrations/20210210093556_AdjustDomainTransactionAndOperation.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TransactionDiscovery.Infrastructure.Persistence;
 
 namespace TransactionDiscovery.Infrastructure.Migrations
 {
     [DbContext(typeof(TdsDbContext))]
-    partial class TdsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210210093556_AdjustDomainTransactionAndOperation")]
+    partial class AdjustDomainTransactionAndOperation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TransactionDiscovery/Migrations/20210210093556_AdjustDomainTransactionAndOperation.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TransactionDiscovery.Infrastructure.Migrations
+{
+	public partial class AdjustDomainTransactionAndOperation : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropColumn(
+				 name: "Fee",
+				 table: "Transactions");
+
+			migrationBuilder.DropColumn(
+				 name: "SequenceNumber",
+				 table: "Transactions");
+
+			migrationBuilder.AddColumn<string>(
+				 name: "Hash",
+				 table: "Transactions",
+				 type: "nvarchar(max)",
+				 nullable: true);
+
+
+			migrationBuilder.AddColumn<string>(
+				name: "Id_Temp",
+				table: "Operations",
+				type: "bigint",
+				nullable: false);
+
+			migrationBuilder.DropUniqueConstraint("PK_Operations", "Operations");
+			migrationBuilder.DropColumn(
+				name: "Id",
+				table: "Operations");
+
+			migrationBuilder.RenameColumn("Id_Temp", "Operations", "Id");
+			migrationBuilder.AddPrimaryKey("PK_Operations", "Operations", "Id");
+
+			migrationBuilder.AddColumn<decimal>(
+				 name: "Amount",
+				 table: "Operations",
+				 type: "decimal(18,2)",
+				 nullable: false,
+				 defaultValue: 0m);
+		}
+
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropColumn(
+				 name: "Hash",
+				 table: "Transactions");
+
+			migrationBuilder.DropColumn(
+				 name: "Amount",
+				 table: "Operations");
+
+			migrationBuilder.AddColumn<decimal>(
+				 name: "Fee",
+				 table: "Transactions",
+				 type: "decimal(18,2)",
+				 nullable: false,
+				 defaultValue: 0m);
+
+			migrationBuilder.AddColumn<long>(
+				 name: "SequenceNumber",
+				 table: "Transactions",
+				 type: "bigint",
+				 nullable: false,
+				 defaultValue: 0L);
+
+			migrationBuilder.AlterColumn<Guid>(
+				 name: "Id",
+				 table: "Operations",
+				 type: "uniqueidentifier",
+				 nullable: false,
+				 oldClrType: typeof(long),
+				 oldType: "bigint")
+				 .OldAnnotation("SqlServer:Identity", "1, 1");
+		}
+	}
+}
